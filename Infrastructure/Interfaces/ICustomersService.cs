36a9b29ca09b8e2e63ddf0efbeca3fc2491cb59e
Saodat-Kh <https://github.com/Saodat-Kh/MyProject.git>
2026-01-05 01:24:15 +0500

    Восстановлены файлы ICustomersService и ISellersService

---
+++ Infrastructure/Interfaces/ICustomersService.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+namespace Infrastructure.Interfaces;
+
+public interface ICustomerService
+{
+    bool CreateCustomer(Customer customer);
+    bool UpdateCustomer(Customer customer);
+    bool DeleteCustomer(int customerId);
+    Customer GetCustomerById(int customerId);
+    List<Customer> GetAllCustomers();
+}
