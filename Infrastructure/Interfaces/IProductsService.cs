adeaa9a47ff27b7cb9f82d13d8f5e2cf854b7c90
Saodat-Kh <https://github.com/Saodat-Kh/MyProject.git>
2026-01-05 11:40:33 +0500

    ptor

--- Infrastructure/Interfaces/IProductsService.cs
+++ Infrastructure/Interfaces/IProductsService.cs
@@ -9,4 +9,8 @@ public interface IProductsService
     bool CreateProduct(Products products);
     bool UpdateProduct(Products products);
     bool DeleteProduct(int id);
+    List<Products> SearchProducts(string name);
+    Products GetProductByCategoryId(int categoryId);
+    Products GetProductBySellerId(int sellerId);
+    
 }
--- Infrastructure/Services/ProductsService.cs
+++ Infrastructure/Services/ProductsService.cs
@@ -115,5 +115,88 @@ public class ProductsService : IProductsService
         return res > 0;
     }
     #endregion
+
+    #region SearchProducts
+    public List<Products> SearchProducts(string name)
+    { 
+        var connection = new NpgsqlConnection(a);
+        connection.Open();
+        const string query = 
+            @"SELECT * FROM Products WHERE name LIKE '%' || @name || '%';";
+        var command = new NpgsqlCommand(query, connection);
+        command.Parameters.AddWithValue("@name", name);
+        var res = command.ExecuteReader();
+        List<Products> products = new List<Products>();
+        while (res.Read())
+        {
+            var product = new Products()
+            {
+                Id = res.GetInt32(res.GetOrdinal("id")),
+                Name = res.GetString(res.GetOrdinal("name")),
+                Price = res.GetInt32(res.GetOrdinal("price")),
+                Quantity = res.GetInt32(res.GetOrdinal("quantity")),
+                Category_id = res.GetInt32(res.GetOrdinal("category_id")),
+                Seller_id = res.GetInt32(res.GetOrdinal("seller_id"))
+            };
+            products.Add(product);
+        }
+        res.Close();
+        return products;
+    }
+    #endregion
+
+    #region GetProductByCategoryId
+    public Products GetProductByCategoryId(int categoryId)
+    {
+        var connection  = new NpgsqlConnection(a);
+        connection.Open();
+        const string query = @"SELECT * FROM Products WHERE Category_id = @category_id;";
+        var command = new NpgsqlCommand(query, connection);
+        command.Parameters.AddWithValue("@category_id", categoryId);
+        var reader = command.ExecuteReader();
+        if (reader.Read())
+        {
+            var product = new Products()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("id")),
+                Name = reader.GetString(reader.GetOrdinal("name")),
+                Price = reader.GetInt32(reader.GetOrdinal("price")),
+                Quantity = reader.GetInt32(reader.GetOrdinal("quantity")),
+                Category_id = reader.GetInt32(reader.GetOrdinal("category_id")),
+                Seller_id = reader.GetInt32(reader.GetOrdinal("seller_id"))
+            };
+            return product;
+        }
+        connection.Close();
+        return null;
+    }
+    #endregion
+
+    #region GetProductBySellerId
+    public Products GetProductBySellerId(int sellerId)
+    {
+        var connection = new  NpgsqlConnection(a);
+        connection.Open();
+        const string query = @"SELECT * FROM Products WHERE seller_id = @seller_id;";
+        var command = new NpgsqlCommand(query, connection);
+        command.Parameters.AddWithValue("@seller_id", sellerId);
+        var reader =  command.ExecuteReader();
+        if (reader.Read())
+        {
+            var product = new Products()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("id")),
+                Name = reader.GetString(reader.GetOrdinal("name")),
+                Price = reader.GetInt32(reader.GetOrdinal("price")),
+                Category_id = reader.GetInt32(reader.GetOrdinal("category_id")),
+                Quantity = reader.GetInt32(reader.GetOrdinal("quantity")),
+                Seller_id = reader.GetInt32(reader.GetOrdinal("seller_id"))
+            };
+            return product;
+        }
+        connection.Close();
+        return null;
+    }
+    #endregion
 }
 
